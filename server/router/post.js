7b9b2958a97c05fa6b04dfc3ac9e7c210a1e3ac1
Gregmar Cruz D.O <49351487+GregmarC@users.noreply.github.com>
2022-12-14 17:33:57 -0500

    added get all posts function

--- server/router/post.js
+++ server/router/post.js
@@ -5,7 +5,6 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { verifyToken } = require("./verifytoken");
-// const Post = require("../models/Post");
 const VerificationToken = require("../models/VerificationToken");
 const JWTSEC = "#2@!@$ndja45883 r7##";
 const ResetToken = require("../models/ResetToken");
@@ -46,6 +45,30 @@ router.post("/create/post",
 
         res.status(200).json({ Status: "Successful", msg: "Post created", post: post._id })
     })
-    
+
+router.get("/getAll",
+    async (req, res) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(400).json(error)
+        }
+        //   try {
+
+        let posts = await Post.find({});
+        // const accessToken = jwt.sign({
+        //     id: user._id,
+        //     username: user.username
+        // }, JWTSEC);
+
+        // const PIN = Math.floor(1000 + Math.random() * 9000);    
+        // const verificationToken = await VerificationToken.create({
+        //     user: user._id,
+        //     token: PIN
+        // });
+        // verificationToken.save();
+
+        res.status(200).json({ posts: posts });
+    })
+
 
 module.exports = router;
