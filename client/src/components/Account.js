470ec3ee0e8ba5c2afc3aee36a1aba013cd9f4f1
Gregmar Cruz D.O <49351487+GregmarC@users.noreply.github.com>
2022-12-17 17:38:07 -0500

    remove work post (client side)

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { delete_cookie } from 'sfcookies'
 import HandyManComponent from "./HandyManComponent";
+import axios from "axios";
 
 const Account = () => {
-  const cookie_key = "navigate"   // clear cookie for navigating to reset page
-  delete_cookie(cookie_key)
-  
   const { user: currentUser } = useSelector((state) => state.auth);
+  const [myPosts, setPosts] = useState(currentUser.other.workPosts);
 
-  let myPosts = currentUser.other.workPosts;
   console.log("My posts: ", myPosts);
+  console.log("Current user: ", currentUser);
+  
+  useEffect(() => {
+  }, [myPosts]);
+  
+  const cookie_key = "navigate"   // clear cookie for navigating to reset page
+  delete_cookie(cookie_key);
+
+  const API_URL = "http://localhost:5000/api/post/";
+  const API_URL_USER = "http://localhost:5000/api/user/";
+  
+
 
   if (!currentUser) {
     return <Navigate to="/login" />;
+  }
+
+
+  const removePost = async (post) => {
+    console.log("Post to be removed: ", post);
+    const id = currentUser.other._id;
+
+    console.log("id: ", id);
+
+    const response = await axios
+        .put((API_URL + `remove/${id}`), {post: post})
+        .catch((error) => console.log('Error: ', error));
+    if (response) {
+        console.log(response);
+        setPosts(response.data.updatedUser.workPosts);
+    }
   }
 
   return (
@@ -26,7 +52,7 @@ const Account = () => {
       </header>
       <div className="myPostsNum">{`My total post count: ${myPosts.length}`}</div>
       {myPosts.map(post => {
-        return <HandyManComponent key={post._id} handyMan={post} canDelete={true}/>
+        return <HandyManComponent key={post._id} handyMan={post} canDelete={true} removePost={removePost}/>
       })}
     </div>
   );
