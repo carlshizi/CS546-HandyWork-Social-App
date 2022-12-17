bce36b1ffbb2f568f35b5db7363825836ea5a2e6
susienite <stan212435@gmail.com>
2022-12-16 21:12:16 -0500

    Landing Page commit

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,14 +1,27 @@
 
+import "./LandingPage.css";
+import sinkpic from "./img/LP-sinkrepair.png";
+import lightpic from "./img/LP-lightrepair.png";
+import windowpic from "./img/LP-windowrepair.jpg";
+
+
 const Landing = () => {
+ 
 
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>Landing Page</strong>
-        </h3>
+    <div className="landing-page">
+      <header>
+        <h3>HandyWork</h3>
+        <p className="purpose">A website designed to connect you to a local, trustworthy handyman.</p>
+        <a href="/Register"  id="registerBtn" class="registerBtn">Sign Up</a>
       </header>
+      <div className="img-container">
+        <img className="homeImg" alt="handyman fixing sink" title="Sink Repairs" src={sinkpic}></img>   
+        <img className="homeImg" alt="handyman fixing lights" title="Light Repairs" src={lightpic}></img>    
+        <img className="homeImg" alt="handyman fixing window" title="Window Repairs"src={windowpic}></img>
+      </div>
     </div>
+
   );
 };
 
