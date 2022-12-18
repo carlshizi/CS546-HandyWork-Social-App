80099d2d19f9069ec89fcabb68caa8c65178886b
susienite <stan212435@gmail.com>
2022-12-17 20:57:54 -0500

    resolve index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import message from "./message";
-import profile from "./profile"; 
 
 export default combineReducers({
   auth,
-  message,
-  profile
+  message
 });
